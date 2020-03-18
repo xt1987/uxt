eabcb9c2368b3c74418f62bd65b92c63a1b8e223
xtcoder <xtcoder@qq.com>
2020-03-18 14:23:33 +0800

    提取按钮mixin

--- uxt/components/uxt-button.vue
+++ uxt/components/uxt-button.vue
@@ -43,9 +43,10 @@
 <script>
 import baseMixin from '@/uxt/mixins/base.js'
 import rrMixin from '@/uxt/mixins/rr.js'
+import btnMixin from '@/uxt/mixins/btn.js'
 
 export default {
-    mixins: [baseMixin, rrMixin],
+    mixins: [baseMixin, rrMixin, btnMixin],
     props: {
         // 大小 'sm'小 ''普通 'lg'大 'block'块状
         size: {
@@ -62,40 +63,6 @@ export default {
         // 镂空
         hollow: {
             type: String
-        },
-        // 禁用
-        disabled: {
-            type: Boolean,
-            default: false
-        },
-        // 开放能力
-        openType: {
-            type: String,
-            default: '',
-            validator(value) {
-                return [
-                    '',
-                    'feedback',
-                    'share',
-                    'getUserInfo',
-                    'contact',
-                    'getPhoneNumber',
-                    'launchApp',
-                    'openSetting',
-                    'getAuthorize',
-                    'contactShare',
-                    'lifestyle',
-                    'openGroupProfile'
-                ].includes(value)
-            }
-        },
-        // 触发form组件submit/reset
-        formType: {
-            type: String,
-            default: '',
-            validator(value) {
-                return ['', 'submit', 'reset'].includes(value)
-            }
         }
     },
     computed: {
@@ -116,26 +83,6 @@ export default {
         // 点击事件
         handleClick(e) {
             this.$emit('click', e)
-        },
-        // 当使用微信小程序开放能力时，发生错误时回调
-        handleError(e) {
-            this.$emit('error', e)
-        },
-        // 微信小程序获取用户手机号回调
-        handleGetPhoneNumber(e) {
-            this.$emit('getphonenumber', e)
-        },
-        // 微信小程序获取用户信息回调
-        handleGetUserInfo(e) {
-            this.$emit('getuserinfo', e)
-        },
-        // 微信小程序打开 APP 成功的回调
-        handleLaunchApp(e) {
-            this.$emit('launchapp', e)
-        },
-        // 微信小程序在打开授权设置页并关闭后回调
-        handleOpenSetting(e) {
-            this.$emit('opensetting', e)
         }
     }
 }
